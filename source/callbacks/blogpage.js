b26dcb532f219818d911186dc5df6ba7dc2ae6b3
Jeff Schwartz <jefftschwartz@gmail.com>
2018-12-08 16:33:26 -0500

    now checking that there are articles to be processed

--- source/callbacks/blogpage.js
+++ source/callbacks/blogpage.js
@@ -3,15 +3,17 @@ const capitalize = require("./lib/capitalize.js");
 module.exports = ({ $, page, site, cheerio }) => {
     const $target = $("#articles");
     $target.after("<ul class=\"articles-list\"></ul>");
-    site.articlesCatalog.forEach(item => {
-        const $articlesList = $("ul.articles-list");
-        const data = item.matter.data;
-        $articlesList.append(/* html */`
+    if (site.articlesCount) {
+        site.articlesCatalog.forEach(item => {
+            const $articlesList = $("ul.articles-list");
+            const data = item.matter.data;
+            $articlesList.append(/* html */`
             <li>
                 <a data-trio-link href="${item.url}">
                     ${data.title} - Posted to ${capitalize(data.category[0])} - ${item.articleDate}
                 </a>
             </li>
         `);
-    });
+        });
+    }
 };
