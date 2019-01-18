7f2d1338ab6ca29c934e0126430114f6afd23314
Jeff Schwartz <jefftschwartz@gmail.com>
2019-01-18 08:51:11 -0500

    refactor callback

--- source/callbacks/blogpage.js
+++ source/callbacks/blogpage.js
@@ -1,19 +1,19 @@
 const capitalize = require("./lib/capitalize.js");
 
-module.exports = ({ $, page, site, cheerio }) => {
-    const $target = $("#articles");
-    $target.after("<ul class=\"articles-list\"></ul>");
+module.exports = ({ $, site }) => {
     if (site.articlesCount) {
+        const $target = $("#articles");
+        $target.after("<ul class=\"articles-list\"></ul>");
         site.articlesCatalog.forEach(item => {
             const $articlesList = $("ul.articles-list");
             const data = item.matter.data;
             $articlesList.append(/* html */`
-            <li>
-                <a data-trio-link href="${item.url}">
-                    ${data.title} - Posted to ${capitalize(data.category[0])} - ${item.articleDate}
-                </a>
-            </li>
-        `);
+                <li>
+                    <a data-trio-link href="${item.url}">
+                        ${data.title} - Posted to ${capitalize(data.category[0])} - ${item.articleDate}
+                    </a>
+                </li>
+            `);
         });
     }
 };
