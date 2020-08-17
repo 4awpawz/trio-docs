79927698595efb8d0cd76b07d09168852503b8ed
Jeff Schwartz <jefftschwartz@gmail.com>
2020-08-17 11:50:47 -0400

    now checking if asset contains social media tags

--- source/callbacks/socialmediametatags.js
+++ source/callbacks/socialmediametatags.js
@@ -1,4 +1,4 @@
 module.exports = ({ asset, $tag, $page }) => {
-    asset.matter.data.socialMediaMetaTags.forEach(metatag => $tag.after(metatag));
+    asset.matter.data.socialMediaMetaTags && asset.matter.data.socialMediaMetaTags.forEach(metatag => $tag.after(metatag));
     $tag.remove();
 };
