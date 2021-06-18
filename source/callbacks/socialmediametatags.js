d3b58ea7f8a9df813451e93385f51cc0fe6dcc93
Jeff Schwartz <jefftschwartz@gmail.com>
2021-06-18 14:12:01 -0400

    now maintaining original order of social media meta tags

--- source/callbacks/socialmediametatags.js
+++ source/callbacks/socialmediametatags.js
@@ -1,4 +1,5 @@
 module.exports = ({ asset, $tag, $page }) => {
-    asset.matter.data.socialMediaMetaTags && asset.matter.data.socialMediaMetaTags.forEach(metatag => $tag.after(metatag));
+    asset.matter.data.socialMediaMetaTags && asset.matter.data.socialMediaMetaTags
+        .forEach(metatag => $tag.before(metatag));
     $tag.remove();
 };
